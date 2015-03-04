fef3f77f9d0b5aa6652dedef2b259624b84a9318
Ivan Babushkin <jogrms@gmail.com>
2015-03-05 01:48:56 +0300

    remove jQuery dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject messenger "0.0.1"
+(defproject messenger "0.0.2"
   :description "Asynchronous window.postMessage library for ClojureScript."
   :url "http://github.com/jogrms/messenger"
   :license {:name "Eclipse Public License 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[jayq "2.5.4"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
+  :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]])
--- src/messenger/core.cljs
+++ src/messenger/core.cljs
@@ -1,7 +1,6 @@
 (ns messenger.core
   (:require [cljs.reader :refer [read-string]]
-            [cljs.core.async :refer [chan put! <!]]
-            [jayq.core :refer [$ on off]])
+            [cljs.core.async :refer [chan put! <!]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (def *origin* nil)
@@ -15,21 +14,26 @@
     resp
     {}))
 
+(defn- add-listener [w f]
+  (.addEventListener w f false))
+
+(defn- remove-listener [w f]
+  (.removeEventListener w f false))
+
 (defn listen [f]
-  (-> ($ js/window)
-      (on "message onmessage"
-        (fn [event]
-          (let [e event.originalEvent]
-            (when (= *origin* (.-origin e))
-              (let [data (read-string e.data)]
-                (when (= (:msg-kind data) :request)
-                  (-> data
-                    (assoc :event e)
-                    (f)
-                    (response)
-                    (merge (select-keys data [:req-id :action]))
-                    (assoc :msg-kind :response)
-                    (post))))))))))
+  (-> js/window
+      (add-listener
+        (fn [e]
+          (when (= *origin* (.-origin e))
+            (let [data (read-string e.data)]
+              (when (= (:msg-kind data) :request)
+                (-> data
+                  (assoc :event e)
+                  (f)
+                  (response)
+                  (merge (select-keys data [:req-id :action]))
+                  (assoc :msg-kind :response)
+                  (post)))))))))
 
 
 (defn listen-async
@@ -39,18 +43,16 @@
 
 
 (defn- get-resp-handler [c action req-id]
-  (fn handler [event]
-    (let [e event.originalEvent]
-      (when (= *origin* (.-origin e))
-        (let [data (read-string e.data)]
-          (when (and
-                  (= action (:action data))
-                  (= req-id (:req-id data))
-                  (= :response (:msg-kind data)))
-            (-> ($ js/window)
-                (off "message onmessage" handler))
-
-            (put! c (assoc data :event e))))))))
+  (fn handler [e]
+    (when (= *origin* (.-origin e))
+      (let [data (read-string e.data)]
+        (when (and
+                (= action (:action data))
+                (= req-id (:req-id data))
+                (= :response (:msg-kind data)))
+          (-> js/window
+              (remove-listener handler))
+          (put! c (assoc data :event e)))))))
 
 (defn request
   ([action] (request action {}))
@@ -58,8 +60,8 @@
     (let [c (chan)
           req-id (js/Math.random)
           handler (get-resp-handler c action req-id)]
-      (-> ($ js/window)
-          (on "message onmessage" handler))
+      (-> js/window
+          (add-listener handler))
       (post (assoc data
               :action action
               :req-id req-id
